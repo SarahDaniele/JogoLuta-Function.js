c90da894575a7038c37e5157f8ea849d7fe9085f
Sarah Dani <sarahdanicm15@gmail.com>
2023-09-01 20:28:03 -0300

    Atualizacao do jogo

--- functions.js
+++ functions.js
@@ -6,7 +6,7 @@ const defaultCharacter = {
     defense: 0
 }
 
-const createKnight =(name) => {
+const createKnight = (name) => {
     return {
         ...defaultCharacter,
         name,
@@ -17,7 +17,7 @@ const createKnight =(name) => {
     }
 }
 
-const createSorcerer =(name) => {
+const createSorcerer = (name) => {
     return {
         ...defaultCharacter,
         name,
@@ -28,10 +28,10 @@ const createSorcerer =(name) => {
     }
 }
 
-const createLittleMonster =(name) => {
+const createLittleMonster = () => {
     return {
         ...defaultCharacter,
-        name: 'LittleMonster',
+        name: 'Little Monster',
         life: 40,
         maxLife: 40,
         attack: 4,
@@ -39,13 +39,79 @@ const createLittleMonster =(name) => {
     }
 }
 
-const createBigMonster =(name) => {
+const createBigMonster = () => {
     return {
         ...defaultCharacter,
-        name: 'BigMonster',
+        name: 'Big Monster',
         life: 100,
         maxLife: 100,
         attack: 16,
         defense: 4
+    }
+}
+
+const stage = {
+
+    start(fighter1, fighter2, fighter1El, fighter2El) {
+        this.fighter1 = fighter1;
+        this.fighter2 = fighter2;
+        this.fighter1El = fighter1El;
+        this.fighter2El = fighter2El;
+
+        this.fighter1El.querySelector('.attackButton').addEventListener('click', () => this.doAttack(this.fighter1, this.fighter2) );
+        this.fighter2El.querySelector('.attackButton').addEventListener('click', () => this.doAttack(this.fighter2, this.fighter1) );
+    
+        this.update();
+    },
+    update() {
+        //Fighter 1
+        this.fighter1El.querySelector('.name').innerHTML = `${this.fighter1.name} - ${this.fighter1.life.toFixed(1)} HP`;
+        let f1Pct = (this.fighter1.life / this.fighter1.maxLife) * 100;
+        this.fighter1El.querySelector('.bar').style.width = `${f1Pct}%`;
+        
+        //Fighter 2
+        this.fighter2El.querySelector('.name').innerHTML = `${this.fighter2.name} - ${this.fighter2.life.toFixed(1)} HP`;
+        let f2Pct = (this.fighter2.life / this.fighter2.maxLife) * 100;
+        this.fighter2El.querySelector('.bar').style.width = `${f2Pct}%`;
+    },
+    doAttack(attacking, attacked) {
+        //console.log(`${attacking.name} atacando ${attacked.name}`);
+        if(attacking.life <= 0){
+            console.log('O jogador nao pode atacar pois esta morto');
+        } else if (attacked.life <= 0){
+            console.log('O jogador ja morreu');
+            return
+        }
+
+        const attackFactor = (Math.random() * 2).toFixed(2);
+        const defenseFactor = (Math.random() * 2).toFixed(2);
+
+        const actualAttack = attacking.attack * attackFactor;
+        const actualDefense = attacked.defense * defenseFactor;
+
+        if(actualAttack > actualDefense){
+            attacked.life -= actualAttack;
+            attacked.life = attacked.life < 0 ? 0 : attacked.life;
+            log.addMessage(`${attacking.name} causou ${actualAttack.toFixed(2)} de dano em ${attacked.name}`);
+        } else {
+            log.addMessage(`${attacked.name} conseguiu defender....`)
+        }
+
+        this.update();
+    }
+}
+
+const log ={
+    list: [],
+    addMessage(msg) {
+        this.list.push(msg);
+        this.render();
+    },
+    render() {
+        const logEl = document.querySelector('.log');
+
+        for(let i in this.list) {
+            logEl.innerHTML += `<li>${this.list[i]}</li>`
+        }
     }
 }
--- script.js
+++ script.js
@@ -1,34 +1,40 @@
-const knight = createKnight('Fulano Knight');
-console.log(knight.name);
-console.log(knight.attack);
-console.log(knight.defense);
-console.log(knight.life);
-console.log(knight.maxLife);
+const char = createKnight('Fulano Knight');
+console.log(char.name);
+console.log(char.attack);
+console.log(char.defense);
+console.log(char.life);
+console.log(char.maxLife);
 
 console.log('------------------------------------------------------')
 
-const sorcerer = createSorcerer('Sicrano Sorcerer');
-console.log(sorcerer.name);
-console.log(sorcerer.attack);
-console.log(sorcerer.defense);
-console.log(sorcerer.life);
-console.log(sorcerer.maxLife);
+// const sorcerer = createSorcerer('Sicrano Sorcerer');
+// console.log(sorcerer.name);
+// console.log(sorcerer.attack);
+// console.log(sorcerer.defense);
+// console.log(sorcerer.life);
+// console.log(sorcerer.maxLife);
 
-console.log('------------------------------------------------------')
+// console.log('------------------------------------------------------')
 
-const littleMonster = createLittleMonster();
-console.log(littleMonster.name);
-console.log(littleMonster.attack);
-console.log(littleMonster.defense);
-console.log(littleMonster.life);
-console.log(littleMonster.maxLife);
+const monster = createLittleMonster();
+console.log(monster.name);
+console.log(monster.attack);
+console.log(monster.defense);
+console.log(monster.life);
+console.log(monster.maxLife);
 
-console.log('------------------------------------------------------')
+// console.log('------------------------------------------------------')
 
-const bigMonster = createBigMonster();
-console.log(bigMonster.name);
-console.log(bigMonster.attack);
-console.log(bigMonster.defense);
-console.log(bigMonster.life);
-console.log(bigMonster.maxLife);
+// const bigMonster = createBigMonster();
+// console.log(bigMonster.name);
+// console.log(bigMonster.attack);
+// console.log(bigMonster.defense);
+// console.log(bigMonster.life);
+// console.log(bigMonster.maxLife);
 
+stage.start(
+    char,
+    monster,
+    document.querySelector('#char'),
+    document.querySelector('#monster')
+)
