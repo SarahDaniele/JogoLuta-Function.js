b45198f8d6693868a487cb3018a06b5f45724aea
Sarah Dani <sarahdanicm15@gmail.com>
2023-09-01 19:02:52 -0300

    Implementando os personagens

--- functions.js
+++ functions.js
@@ -1,4 +1,51 @@
 const defaultCharacter = {
+    name: '',
     life: 1,
-    maxLife
+    maxLife: 1,
+    attack: 0,
+    defense: 0
+}
+
+const createKnight =(name) => {
+    return {
+        ...defaultCharacter,
+        name,
+        life: 100,
+        maxLife: 100,
+        attack: 10,
+        defense: 8
+    }
+}
+
+const createSorcerer =(name) => {
+    return {
+        ...defaultCharacter,
+        name,
+        life: 50,
+        maxLife: 50,
+        attack: 15,
+        defense: 3
+    }
+}
+
+const createLittleMonster =(name) => {
+    return {
+        ...defaultCharacter,
+        name: 'LittleMonster',
+        life: 40,
+        maxLife: 40,
+        attack: 4,
+        defense: 5
+    }
+}
+
+const createBigMonster =(name) => {
+    return {
+        ...defaultCharacter,
+        name: 'BigMonster',
+        life: 100,
+        maxLife: 100,
+        attack: 16,
+        defense: 4
+    }
 }
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let knight = new Knight('Fulano Knight');
+const knight = createKnight('Fulano Knight');
 console.log(knight.name);
 console.log(knight.attack);
 console.log(knight.defense);
@@ -7,7 +7,7 @@ console.log(knight.maxLife);
 
 console.log('------------------------------------------------------')
 
-let sorcerer = new Sorcerer('Sicrano Sorcerer');
+const sorcerer = createSorcerer('Sicrano Sorcerer');
 console.log(sorcerer.name);
 console.log(sorcerer.attack);
 console.log(sorcerer.defense);
@@ -16,7 +16,7 @@ console.log(sorcerer.maxLife);
 
 console.log('------------------------------------------------------')
 
-let littleMonster = new LittleMonster();
+const littleMonster = createLittleMonster();
 console.log(littleMonster.name);
 console.log(littleMonster.attack);
 console.log(littleMonster.defense);
@@ -25,25 +25,10 @@ console.log(littleMonster.maxLife);
 
 console.log('------------------------------------------------------')
 
-let bigMonster = new BigMonster();
+const bigMonster = createBigMonster();
 console.log(bigMonster.name);
 console.log(bigMonster.attack);
 console.log(bigMonster.defense);
 console.log(bigMonster.life);
 console.log(bigMonster.maxLife);
 
-console.log('------------------------------------------------------')
-
-let log = new Log(document.querySelector('.log'));
-
-
-const stage = new Stage(
-    sorcerer,
-    littleMonster,
-    document.querySelector('#char'),
-    document.querySelector('#monster'),
-    log
-
-);
-
-stage.start();
